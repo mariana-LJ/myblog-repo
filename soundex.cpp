01cd5e30703af2aaf811c5bab52f0c1e9f351b9f
mariana-LJ <mariana.lopez.jaimez@gmail.com>
2014-12-26 21:22:36 -0600

    Optimizing soundex implementation

--- soundex.cpp
+++ soundex.cpp
@@ -9,135 +9,134 @@
 */
 
 #include <iostream>
-#include <string>
-#include <vector>
-#include <sstream>
+#include <cstring>
+
+typedef unsigned char uint8_t;
 
 using namespace std;
 
-string convertSurname(vector<std::string> &codes, string surname);
-void printSoundex(string sourname, string s);
-void initSurnameCodes(vector<std::string> &codes);
-string toUppercase(string surname);
-string deleteDuplicates(string &s);
-string removeZeros(string &s);
-string adjustSize(string &s);
+void convertSurname(uint8_t *codes, char *surname);
+void initSurnameCodes(uint8_t *codes);
+void deleteDuplicates(char *s);
+void removeZeros(char *s);
+void adjustSize(char *s);
 
 int main(int argc, char* argv[])
 {
-   vector<std::string> surnameCodes;
-   string soundexCode = "";
-   char * surname;
-
-   surname = argv[1];
+   uint8_t surnameCodes[256];
+   char * surname = argv[1];
 
    initSurnameCodes(surnameCodes);
-
-   cout << toUppercase(surname) << endl;
-   soundexCode = convertSurname(surnameCodes, surname);
-   soundexCode = deleteDuplicates(soundexCode);
-   soundexCode = removeZeros(soundexCode);
-   soundexCode = adjustSize(soundexCode);
-   printSoundex(surname, soundexCode);
+   convertSurname(surnameCodes, surname);
+   deleteDuplicates(surname);
+   removeZeros(surname);
+   adjustSize(surname);
+   cout << surname << endl;
    
    return 0;
 }
 
-string deleteDuplicates(string &s){
-   stringstream ss;
-
-   ss << s[0];
-   ss << s[1];
+void deleteDuplicates(char *s){
+   char *last = s + 1;
+   char *current = last + 1;
    
-   for(int i = 2; i < s.length(); ++i){
-      if(s[i] != s[i-1]){
-         ss << s[i];
+   for(; *current; ++current){
+      if(*current != *(current-1)){
+         last++;
+         *last = *current;
       }
    }
-
-   return ss.str();
-}
-
-string removeZeros(string &s){
-   stringstream ss;
-   int i = 0;
-
-   while(i < s.length()){
-      if(s[i] != '0'){
-         ss << s[i];
-      }
-      ++i;
-   }
-
-   return ss.str();
-}
-
-string adjustSize(string &s){
-   stringstream ss;
-   ss << s;
-   int correctSize = 4;
-   int i = ss.str().length();
-
-   while(i < correctSize){
-      ss << '0';
-      i = ss.str().length();
-   }
-
-   if(i > correctSize){
-      return ss.str().substr(0,correctSize);
-   }
-   else{
-      return ss.str();
-   }
-}
-
-string toUppercase(string surname)
-{
-   stringstream ss;
-
-   for(std::string::iterator it=surname.begin(); it!=surname.end(); ++it){
-      if((*it > 96) && (*it < 123)){ // if char *it is lowercase (ASCII code)
-         // The difference between upper and lower case in ASCII:
-         ss << char(*it -32);
-      } else if((*it >64) && (*it < 91)){
-         ss << *it; // append char if it's already uppercase
-      }
-   }
-
-   return ss.str();
-}
-
-void initSurnameCodes(vector<std::string> &codes)
-{
-   codes.push_back("AEIOUHWY");
-   codes.push_back("BFPV");
-   codes.push_back("CGJKQSXZ");
-   codes.push_back("DT");
-   codes.push_back("MN");
-   codes.push_back("L");
-   codes.push_back("R");
-}
-
-void printSoundex(string surname, string code)
-{
-   cout << "Soundex code for " << surname << " is " << code << endl;
-}
-
-string convertSurname(vector<std::string> &codes, string surname)
-{
-   string upperCaseSurname = toUppercase(surname);
-   stringstream ss;
-
-   ss << upperCaseSurname[0]; 
    
-   for(int c=1; c<upperCaseSurname.length(); ++c){
-      for(int index=0; index<codes.size(); ++index){
-         for(int strIndex=0; strIndex<codes[index].length(); ++strIndex){
-            if(upperCaseSurname[c] == codes[index][strIndex]){
-               ss << index;
-            }
-         }
+   last++;
+   last = '\0'; // end of string
+}
+
+void removeZeros(char *s){
+   char *last = s + 1;
+   char *current = s + 1;
+   
+   for(; *current; ++current) {
+      if(*current != '0') {
+         *last = *current;
+         last++;
       }
    }
-   return ss.str();
+   last = '\0';
+}
+
+void adjustSize(char *s){
+   size_t len = strlen(s);
+   const int correctSize = 4;
+   
+   for(; len < correctSize; ++len) {
+      s[len] = '0';
+   }
+   
+   s[correctSize] = '\0';
+}
+
+void initSurnameCodes(uint8_t *codes)
+{
+   codes['A'] = 0;
+   codes['E'] = 0;
+   codes['I'] = 0;
+   codes['O'] = 0;
+   codes['U'] = 0;
+   codes['H'] = 0;
+   codes['W'] = 0;
+   codes['Y'] = 0;
+   codes['B'] = 1;
+   codes['F'] = 1;
+   codes['P'] = 1;
+   codes['V'] = 1;
+   codes['C'] = 2;
+   codes['G'] = 2;
+   codes['J'] = 2;
+   codes['K'] = 2;
+   codes['Q'] = 2;
+   codes['S'] = 2;
+   codes['X'] = 2;
+   codes['Z'] = 2;
+   codes['D'] = 3;
+   codes['T'] = 3;
+   codes['M'] = 4;
+   codes['N'] = 4;
+   codes['L'] = 5;
+   codes['R'] = 6;
+
+   codes['a'] = 0;
+   codes['e'] = 0;
+   codes['i'] = 0;
+   codes['o'] = 0;
+   codes['u'] = 0;
+   codes['h'] = 0;
+   codes['w'] = 0;
+   codes['y'] = 0;
+   codes['b'] = 1;
+   codes['f'] = 1;
+   codes['p'] = 1;
+   codes['v'] = 1;
+   codes['c'] = 2;
+   codes['g'] = 2;
+   codes['j'] = 2;
+   codes['k'] = 2;
+   codes['q'] = 2;
+   codes['s'] = 2;
+   codes['x'] = 2;
+   codes['z'] = 2;
+   codes['d'] = 3;
+   codes['t'] = 3;
+   codes['m'] = 4;
+   codes['n'] = 4;
+   codes['l'] = 5;
+   codes['r'] = 6;
+}
+
+void convertSurname(uint8_t *codes, char *surname)
+{
+   surname[0] = toupper(surname[0]);
+   
+   for(int i = 1; surname[i]; ++i) {
+      surname[i] = codes[surname[i]] + '0';
+   }
 }
